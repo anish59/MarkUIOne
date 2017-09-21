e1f09025af62051c787512722b53868a067496e8
anish <anish.vahora5@gmail.com>
2017-09-21 12:55:39 +0530

    dkdk

--- app/src/main/java/com/markuione/fragmentsDemo/TabActivity.java
+++ app/src/main/java/com/markuione/fragmentsDemo/TabActivity.java
@@ -6,6 +6,10 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.markuione.R;
+import com.markuione.fragmentsDemo.adpater.ViewPagerAdapter;
+import com.markuione.fragmentsDemo.fragments.FragOne;
+import com.markuione.fragmentsDemo.fragments.FragThree;
+import com.markuione.fragmentsDemo.fragments.FragTwo;
 
 public class TabActivity extends AppCompatActivity {
 
@@ -17,13 +21,20 @@ public class TabActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab);
         init();
-
-
     }
 
     private void init() {
         this.viewpager = (ViewPager) findViewById(R.id.viewpager);
         this.tabs = (TabLayout) findViewById(R.id.tabs);
+        setUpViewPager();
+        tabs.setupWithViewPager(viewpager);
+    }
 
+    private void setUpViewPager() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FragOne(), "One");
+        adapter.addFragment(new FragTwo(), "Two");
+        adapter.addFragment(new FragThree(), "Three");
+        viewpager.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/markuione/fragmentsDemo/adpater/ViewPagerAdapter.java
+++ app/src/main/java/com/markuione/fragmentsDemo/adpater/ViewPagerAdapter.java
@@ -4,22 +4,38 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by anish on 20-09-2017.
  */
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> mFragmentList = new ArrayList<>();
+    private List<String> mFragmentTitleList = new ArrayList<>();
+
     public ViewPagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        return mFragmentList.get(position);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
     }
 }
